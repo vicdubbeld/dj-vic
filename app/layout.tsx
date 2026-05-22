e2b83d69bf79c2b55867b63b301dc6a771273b9e
vicdubbeld <vicdubbeld@gmail.com>
2026-05-22 13:05:25 -0700

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'V!C — DJ · Portland, OR',
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider>{children}</ThemeProvider>
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
