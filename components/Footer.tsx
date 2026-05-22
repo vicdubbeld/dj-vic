61f8db1b2a46663750d67db5266054560ef995ae
vicdubbeld <vicdubbeld@gmail.com>
2026-05-22 12:32:46 -0700

    added footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.logo}>V<span>!</span>C</div>
       <div className={styles.links}>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">YouTube</a>
-        <a href="https://discogs.com" target="_blank" rel="noopener noreferrer">Discogs</a>
+        <a href="https://www.instagram.com/vic__dj/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://www.youtube.com/@dj__vic" target="_blank" rel="noopener noreferrer">YouTube</a>
+        <a href="https://www.discogs.com/user/vicdubbeld" target="_blank" rel="noopener noreferrer">Discogs</a>
       </div>
     </footer>
   )
